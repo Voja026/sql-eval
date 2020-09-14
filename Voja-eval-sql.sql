5e0d4bb493f7637a592a1001e50c66cf7f156c79
Vojislav  Andelkovic <66475918+Voja026@users.noreply.github.com>
2020-09-14 17:30:35 +0200

    Add files via upload

---
+++ Voja-eval-sql.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Reference (
+    Niveau integer PRIMARY KEY,
+    Vert TEXT,
+    Bleu TEXT,
+    Violet TEXT,
+);
+
+CREATE TABLE Competences (
+    C1 text,
+    C2 text,
+    C3 text,
+);
+
+INSERT INTO (C1,C2,C3) VALUES ('Fatima', 'Deborah'),('Fatima','Deborah','Steeven','Camile'),('')
+
